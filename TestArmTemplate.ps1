a0916a8ff68976a603cb826c52e9ad6973a09724
Chris Mitchell (DX) <chmitch@microsoft.com>
2017-06-08 14:28:12 -0400

    Adding custom deployment script logic

--- TestArmTemplate.ps1
+++ TestArmTemplate.ps1
@@ -4,7 +4,7 @@
 $subscription = "Microsoft Azure Internal Consumption"
 
 #this prefix is used to help make names of vms storage accounts etc unique.  Keep if 5 characters or less.
-$envPrefixName = "cgmtet5" 
+$envPrefixName = "cgmtet7" 
 $username = "amadmin"
 $rgname = $envPrefixName + "rg"
 
