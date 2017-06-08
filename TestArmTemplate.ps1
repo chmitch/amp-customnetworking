1c4da7646e41f9e037edef262af7e9741257f28d
Chris Mitchell (DX) <chmitch@microsoft.com>
2017-06-08 17:18:48 -0400

    removing customdata as it is unnecessary

--- TestArmTemplate.ps1
+++ TestArmTemplate.ps1
@@ -4,7 +4,7 @@
 $subscription = "Microsoft Azure Internal Consumption"
 
 #this prefix is used to help make names of vms storage accounts etc unique.  Keep if 5 characters or less.
-$envPrefixName = "cgmtet7" 
+$envPrefixName = "cgmtet8" 
 $username = "amadmin"
 $rgname = $envPrefixName + "rg"
 
